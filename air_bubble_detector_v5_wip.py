edce52b3622e1c679ab04c34090f254d66594f3d
Carlo <carlonuebel@web.de>
2024-02-14 15:39:50 +0100

    progress, memory of tracker, parameter optimization

--- air_bubble_detector_v5_wip.py
+++ air_bubble_detector_v5_wip.py
@@ -4,16 +4,13 @@ import matplotlib.pyplot as plt
 from object_tracker import *
 
 # Constants
-VIDEO_PATH = r'C:\Users\Carlo\Repos\floaty_bubbles\vids\0_60_0_60_0-10-1-24.avi'
+VIDEO_PATH = r'C:\Users\Carlo\Repos\floaty_bubbles\vids\0_0_60_0_0-10-01-24.avi'
 OUTPUT_VIDEO_NAME = 'valves_1_3_5.avi'
 WIN_SIZE = (15, 15)
 
 # Dil 5 and ero 15 has the cleanest size graph
-MORPH_KERNEL_SIZE_DILATION = 5 
-MORPH_KERNEL_SIZE_EROSION = 15 
-
-# Create tracker object
-tracker = EuclideanDistTracker()
+MORPH_KERNEL_SIZE_DILATION = 7 
+MORPH_KERNEL_SIZE_EROSION = 19
 
 #object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
@@ -24,6 +21,9 @@ cap = cv2.VideoCapture(VIDEO_PATH)
 fps = cap.get(cv2.CAP_PROP_FPS)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Create tracker object
+tracker = EuclideanDistTracker(width)
 
 # Ignore bottom 20 mm because of the bottom video properties when 
 # playing the video
@@ -37,17 +37,21 @@ output_video = cv2.VideoWriter(OUTPUT_VIDEO_NAME, fourcc, fps,
                                (width, height - ignore_region_height))
 
 # Scale bar length in pixels (adjust as needed)
-scale_bar_length = 100
+scale_bar_length = 8
 
 # Lists to store data for the diagram
 average_sizes_contours = []
 num_contours_per_frame = []
+ids_and_sizes = {}
+first_detected_frame = {}
 
 # Read the first frame from the video
 ret, _ = cap.read()
 
 # Counter for frame number
 frame_number = 0  
+
+#print("y size px: " + str(width))
 
 while True:
     ret, frame = cap.read()
@@ -57,7 +61,7 @@ while True:
     # Apply ROI
     frame = frame[roi[1]:roi[3], roi[0]:roi[2]]
 
-    #frame = cv2.GaussianBlur(frame, (11, 11), 0)
+    #frame_gauss = cv2.GaussianBlur(frame, (25, 25), 0)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -67,13 +71,14 @@ while True:
     # Morphological Operations (Dilation followed by Erosion)
     morph_kernel_dil = np.ones((MORPH_KERNEL_SIZE_DILATION, MORPH_KERNEL_SIZE_DILATION), np.uint8)
     morph_kernel_ero = np.ones((MORPH_KERNEL_SIZE_EROSION, MORPH_KERNEL_SIZE_EROSION), np.uint8)
-    morphed_thresh = cv2.dilate(thresh, morph_kernel_dil, iterations=1)
-    morphed_thresh = cv2.erode(morphed_thresh, morph_kernel_ero, iterations=1)
+
+    morphed_thresh_dil = cv2.dilate(thresh, morph_kernel_dil, iterations=1)
+    morphed_thresh_ero = cv2.erode(morphed_thresh_dil, morph_kernel_ero, iterations=1)
 
     #morphed_thresh = cv2.GaussianBlur(morphed_thresh, (21, 21), 0)
 
     # Find contours in the inverse of the morphed thresholded image
-    contours, _ = cv2.findContours(~morphed_thresh, 
+    contours, _ = cv2.findContours(~morphed_thresh_ero, 
                                    cv2.RETR_EXTERNAL, 
                                    cv2.CHAIN_APPROX_SIMPLE)
     detections = []
@@ -142,6 +147,12 @@ while True:
         average_sizes_contours.append(average_size_contour)
         num_contours_per_frame.append(len(contours_for_counting))
         
+        for object_id, size in zip(objects_ids, average_sizes_contours):
+            _, _, id = object_id
+            if id not in ids_and_sizes:
+                ids_and_sizes[id] = size
+                first_detected_frame[id] = frame_number
+
         # Display live numbers on the frame
         cv2.putText(frame, f"Average Size Contours/Frame: {average_size_contour:.2f} px^2", 
                     (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
@@ -151,15 +162,19 @@ while True:
                      0.8, (255, 255, 255), 2)
 
     # Add a scale bar at the top-right corner
-    scale_bar_start = (width - 10 - scale_bar_length, 10)
-    scale_bar_end = (width - 10, 10)
+    scale_bar_start = (width-307 - scale_bar_length, 605)
+    scale_bar_end = (width-307, 605)
+
+    #scale_bar_start = (width - 10 - scale_bar_length, 10)
+    #scale_bar_end = (width - 10, 10)
+
     cv2.line(frame, scale_bar_start, scale_bar_end, (255, 255, 255), 2)
 
     # Save Processed Frame to Output Video
     output_video.write(frame)
 
     # Visualization
-    cv2.imshow('preprocessing', ~morphed_thresh)
+    cv2.imshow('preprocessing', ~morphed_thresh_ero)
     cv2.imshow('Processed Frame', frame)
 
     key = cv2.waitKey(30)
@@ -183,6 +198,9 @@ plt.plot(average_sizes_contours, label='Average Size Contours')
 plt.title('Average Size of Contours Over Time')
 plt.xlabel('Frame Number')
 plt.ylabel('Size (px^2)')
+# Annotate the plot with IDs at the corresponding frame on the x-axis where they are first detected
+for id, size in ids_and_sizes.items():
+    plt.text(first_detected_frame[id], size, str(id), fontsize=14, color='red')
 plt.legend()
 
 plt.subplot(2, 1, 2)
--- object_tracker.py
+++ object_tracker.py
@@ -1,15 +1,19 @@
 import math
 
 class EuclideanDistTracker:
-    def __init__(self):
+    def __init__(self, vid_width):
         # Store the center positions of the objects
         self.center_points = {}
         self.center_points_1 = {}
         self.center_points_2 = {}
         self.center_points_3 = {}
+        self.center_points_4 = {}
+        self.center_points_5 = {}
         # Keep the count of the IDs
         # each time a new object id detected, the count will increase by one
         self.id_count = 0
+        self.vid_width = vid_width
+        #print(str(self.vid_width))
 
     def update(self, objects_centers):
         # Objects boxes and ids
@@ -26,18 +30,17 @@ class EuclideanDistTracker:
 
                 if dist < 100:
                     self.center_points[id] = (cx, cy)
-                    print(self.center_points)
+                    #print(self.center_points)
                     objects_ids.append((cx, cy, id))
                     same_object_detected = True
                     break
-
 
             for id, pt in self.center_points_1.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
 
                 if dist < 100:
                     self.center_points[id] = (cx, cy)
-                    print(self.center_points)
+                    #print(self.center_points)
                     objects_ids.append((cx, cy, id))
                     same_object_detected = True
                     break
@@ -47,7 +50,7 @@ class EuclideanDistTracker:
 
                 if dist < 100:
                     self.center_points[id] = (cx, cy)
-                    print(self.center_points)
+                    #print(self.center_points)
                     objects_ids.append((cx, cy, id))
                     same_object_detected = True
                     break
@@ -57,14 +60,34 @@ class EuclideanDistTracker:
 
                 if dist < 100:
                     self.center_points[id] = (cx, cy)
-                    print(self.center_points)
+                    #print(self.center_points)
+                    objects_ids.append((cx, cy, id))
+                    same_object_detected = True
+                    break
+
+            for id, pt in self.center_points_4.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+
+                if dist < 100:
+                    self.center_points[id] = (cx, cy)
+                    #print(self.center_points)
+                    objects_ids.append((cx, cy, id))
+                    same_object_detected = True
+                    break
+
+            for id, pt in self.center_points_5.items():
+                dist = math.hypot(cx - pt[0], cy - pt[1])
+
+                if dist < 100:
+                    self.center_points[id] = (cx, cy)
+                    #print(self.center_points)
                     objects_ids.append((cx, cy, id))
                     same_object_detected = True
                     break
 
 
             # New object is detected we assign the ID to that object
-            if same_object_detected is False:
+            if same_object_detected is False and cy > self.vid_width*2/3:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_ids.append((cx, cy, self.id_count))
                 self.id_count += 1
@@ -77,7 +100,8 @@ class EuclideanDistTracker:
             new_center_points[object_id] = center
 
         # Update dictionary with IDs not used removed
-            
+        self.center_points_5 = self.center_points_4.copy()
+        self.center_points_4 = self.center_points_3.copy()
         self.center_points_3 = self.center_points_2.copy()
         self.center_points_2 = self.center_points_1.copy()
         self.center_points_1 = self.center_points.copy()
